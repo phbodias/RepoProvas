f64eb1993a755c8e27130631c5766ed93ffeae78
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-09-18 21:45:20 -0300

    Register tests

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -2,13 +2,13 @@ import Joi from "joi";
 
 const registerSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(10).required(),
+  password: Joi.string().min(6).required(),
   confirmPassword: Joi.ref("password"),
 });
 
 const signinSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(10).required(),
+  password: Joi.string().min(6).required(),
 });
 
 export {registerSchema, signinSchema};
--- tests/user.test.ts
+++ tests/user.test.ts
@@ -16,5 +16,46 @@ describe("Testing POST /signup", () => {
     const body = await userFactory();
     const result = await supertest(app).post("/signup").send(body);
     expect(result.status).toBe(201);
+    expect(result.body.createdAt.length).toBeGreaterThan(0);
+  });
+
+  it("Tries to insert user with email already registered in the database and receives status 409", async () => {
+    const body = await userFactory();
+    await supertest(app).post("/signup").send(body);
+    const result = await supertest(app).post("/signup").send(body);
+    expect(result.status).toBe(409);
+  });
+
+  it("Tries to insert user without email and receives status 422", async () => {
+    const body = {
+      email: "agoogle.com",
+      password: "aaaaaaaaaaaaaa",
+      confirmPassword: "aaaaaaaaaaaaaa",
+    };
+
+    const result = await supertest(app).post("/signup").send(body);
+    expect(result.status).toBe(422);
+  });
+
+  it("Tries to insert user without password and receives status 422", async () => {
+    const body = {
+      email: "agoogle.com",
+      password: "",
+      confirmPassword: "",
+    };
+
+    const result = await supertest(app).post("/signup").send(body);
+    expect(result.status).toBe(422);
+  });
+
+  it("Tries to insert user with confirmPassword different than password and receives status 422", async () => {
+    const body = {
+      email: "agoogle.com",
+      password: "aaaaaaaaaaaaaa",
+      confirmPassword: "bbbbbbbbbbbb",
+    };
+
+    const result = await supertest(app).post("/signup").send(body);
+    expect(result.status).toBe(422);
   });
 });
